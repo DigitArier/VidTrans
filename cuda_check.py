cb33561a82a2402b3f44075c81241358446c6108
DigitArier <regme@gmx.biz>
2025-05-23 20:36:58 +0200

    Remove cuda_check.py as it is no longer needed

--- cuda_check.py
+++
@@ -1,7 +0,0 @@
-import torch
-print(torch.cuda.is_available())
-print(torch.version.cuda)
-print(torch.cuda.get_device_name(0) if torch.cuda.is_available() else "No GPU")
-print(torch.backends.cudnn.version())
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(device)
